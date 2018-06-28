365bbbbe8f51c5502313c34272a5c44767fd8760
btokraks22 <bree.v.v.13@gmail.com>
2018-06-28 17:40:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,65 @@
-class ApplicationController < Sinatra::Base
+require './config/environment'
+require './app/models/model'
 
-  configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
+class ApplicationController < Sinatra::Base
+ configure do
+  	set :public_folder, 'public'
+  	set :views, 'app/views'
   end
 
   get "/" do
-  	erb :index
+    return erb :index
   end
+  
+  post '/' do 
+    @the_fortune=get_fortune
+    @the_user=params[:user]
+    return erb :results
+  end 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # end
+# # get "/newpage" do
+# #   erb :newpage 
+# # end 
+
+# get "/get_name" do 
+#   first_dog = Dog.new("rudolph", "Mastiff", 2 )
+#   second_dog = Dog.new("Charlotte", "Labrador Retriever", 6)
+#   third_dog = Dog.new("Dottie", "Chihuahua Pug Mix" "3 months")
+  
+  
+#   first_dog.get_name
+#   second_dog.get_name
+#   third_dog.get_name
+# end
+
+# get "/get_breed" do
+#   first_dog.get_breed
+#   second_dog.get_breed
+#   third_dog.get_breed
+# end
+
+# get "/get_age" do
+#   first_dog.get_age
+#   second_dog.get_age
+#   third_dog.get_age
+# end
+# end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,21 @@
-class Model
-  # Replace with Dog clas
-end
+def get_fortune
+  
+  fortune_array = ["You will achieve your dreams today", "You'll have a fun party with some old friends", "You will come across a field of Bok Choy"]
+
+  return fortune_array.sample
+end   
+  
+ # puts get_fortune
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+# fortune = fortune_array.sample
+# fortune = fortune_array.sample
+#   fortune_array.delete(fortune)
